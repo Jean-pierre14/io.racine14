18a24267c865fd06ced9af2d942fd4675573ec0e
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-01-03 08:18:33 +0100

    Git ignore

---
+++ server.js
@@ -0,0 +1,15 @@
+const exp = require('express'),
+    cors = require('cors'),
+    {success, error} = require('consola'),
+    app = exp()
+    PORT = process.env.PORT || 7000
+
+app.use(cors())
+app.get('/', (req, res)=>{
+    res.json('Hello')
+})
+
+app.listen(PORT, (e)=>{
+    if(e) error({message: `Error ${e}`, badge: true})
+    success({message: `Server run on port ${PORT}`, badge: true})
+})

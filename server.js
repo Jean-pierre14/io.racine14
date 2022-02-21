1cf71d75685db127734dd272fd00b8f469bb0db4
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-02-21 14:10:33 +0100

    UI UX

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const exp = require("express"),
-  cors = require("cors"),
-  { success, error } = require("consola"),
-  app = exp();
+    cors = require("cors"),
+    { success, error } = require("consola"),
+    app = exp();
 
 require("dotenv").config();
 
@@ -10,19 +10,19 @@ PORT = process.env.PORT || 7000;
 const con = require("./models/db.con");
 
 con.connect((e) => {
-  if (e) throw error;
-  success({ message: `DB connected`, badge: true });
+    if (e) throw error;
+    success({ message: `DB connected`, badge: true });
 });
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("Hello");
+    res.json("Hello");
 });
 
 app.use("/users", require("./routers/"));
 
 app.listen(PORT, (e) => {
-  if (e) error({ message: `Error ${e}`, badge: true });
-  success({ message: `Server run on port ${PORT}`, badge: true });
+    if (e) error({ message: `Error ${e}`, badge: true });
+    success({ message: `Server run on port ${PORT}`, badge: true });
 });

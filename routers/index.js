bb078e45df56b6e7b3730f58a18e25381806708f
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-02-26 09:18:43 +0100

    Var const and date()

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router(),
-  db = require("../models/db.con");
+    db = require("../models/db.con");
 
 let d = new Date();
 const ThisYear = d.getFullYear();
+var Moment = ThisYear - 1,
+    thisyearNormal = Moment + '-' + ThisYear
 
 router.get("/", (req, res) => {
-  let sql = "SELECT * FROM student ORDER BY id_student WHERE AnneeScolaire = ?";
-  db.query(sql, [ThisYear]);
+    let sql = "SELECT * FROM student ORDER BY id_student WHERE AnneeScolaire = ?";
+    db.query(sql, [thisyearNormal]);
 });
 
 module.exports = router;

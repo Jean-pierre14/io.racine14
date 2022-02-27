6d384bb3faf2506a73a966f6a942165b3ce4898d
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-02-27 09:24:10 +0100

    Synthax

--- routers/index.js
+++ routers/index.js
@@ -2,13 +2,18 @@ const router = require("express").Router(),
     db = require("../models/db.con");
 
 let d = new Date();
+
 const ThisYear = d.getFullYear();
+
 var Moment = ThisYear - 1,
     thisyearNormal = Moment + '-' + ThisYear
 
 router.get("/", (req, res) => {
+
     let sql = "SELECT * FROM student ORDER BY id_student WHERE AnneeScolaire = ?";
+
     db.query(sql, [thisyearNormal]);
+
 });
 
 module.exports = router;

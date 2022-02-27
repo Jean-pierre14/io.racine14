b0f9163b915a4de21e2f24740f8430cfdf6839f8
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-02-27 19:29:30 +0100

    Synthax

--- server.js
+++ server.js
@@ -10,23 +10,32 @@ PORT = process.env.PORT || 7000;
 const con = require("./models/db.con");
 
 con.connect((e) => {
+
     if (e) throw error;
     success({ message: `DB connected`, badge: true });
+
 });
 
 const title = 'Racine-14'
 
 app.use(cors());
+
 app.set('view engine', 'ejs')
+
 app.use(exp.static('public/'))
 
 app.get("/", (req, res) => {
+
     res.render('index', { title })
+
 });
 
 app.use("/users", require("./routers/"));
 
 app.listen(PORT, (e) => {
+
     if (e) error({ message: `Error ${e}`, badge: true });
+
     success({ message: `Server run on port ${PORT}`, badge: true });
+
 });
